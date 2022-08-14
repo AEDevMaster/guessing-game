4615a2af07174971dbb2c588a87bcdcbe8d067b3
Anastasiya <ermolenko.anastasiya.work@gmail.com>
2022-08-14 23:17:49 +0300

    fix: GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.middle = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
+        console.log(this.min, this.max)
+        this.middle = Math.round((this.max + this.min) / 2)
 
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
